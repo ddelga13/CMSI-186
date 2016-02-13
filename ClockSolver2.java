61e6106b2a85bc100a184d2e2ebe290ee57f087b
ddelga13 <ddelga13@lion.lmu.edu>
2016-02-13 13:27:45 -0800

    Create ClockSolver2.java

---
+++ ClockSolver2.java
@@ -0,0 +1,26 @@
+class ClockSolver2 {
+ public static void main (String [] args) throws Exception {
+ double slice = 1;
+ double givenAngle = 180;
+  try{
+   slice = Double.parseDouble (args [0]);
+   givenAngle = Double.parseDouble (args [1]);
+  } catch (Exception e){
+   System.out.println ("no argument given.");
+  }
+  Clock clock = new Clock(0.0, 0.0, 0.0, slice, givenAngle);
+   System.out.println ("slice: " + slice);
+   System.out.println ("Current time: " + clock);
+   System.out.println ("Given angle: " + givenAngle);
+   Boolean alreadyPrinted = true;
+   for (int i = 0; i < 60*60*12/slice; i++){
+    clock.tick();
+    if (Math.abs(clock.getAngleBetween() - givenAngle) < (slice/20)){
+     if(alreadyPrinted){System.out.println ("Current time: " + clock + " Current angle: " + clock.getAngleBetween());}
+     alreadyPrinted = false;
+       }else{
+        alreadyPrinted = true;
+       } 
+   }
+  }
+}
