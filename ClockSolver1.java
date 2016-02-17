40e216c52ab2462f811d928c0639acf5bcd7e376
ddelga13 <ddelga13@lion.lmu.edu>
2016-02-16 16:30:07 -0800

    Update ClockSolver1.java

--- ClockSolver1.java
+++ ClockSolver1.java
@@ -1,25 +1,27 @@
 class ClockSolver1 {
- public static void main (String [] args) throws Exception {
- double slice = 60;  
-  try{ // an if statement or conditional statement: if you have args[0], that is your time slice, else slice = 60
+ public static void main(String[] args) throws Exception {
+  double slice = 60;
+  try { // an if statement or conditional statement: if you have args[0], that is your time slice, else slice = 60
    // check for positive reals less than 1800
-   slice = Double.parseDouble (args [0]);
-  } catch (Exception e){
-   System.out.println ("no argument given.");
+   slice = Double.parseDouble(args[0]);
+  } catch (Exception e) {
+   System.out.println("no argument given.");
   }
-Clock clock = new Clock(0.0, 0.0, 0.0, slice);
-  System.out.println ("slice: " + slice + "s");
-  System.out.println ("Current time: " + clock);
+  Clock clock = new Clock(0.0, 0.0, 0.0, slice);
+  System.out.println("slice: " + slice + "s");
+  System.out.println("Current time: " + clock);
   Boolean alreadyPrinted = true;
-  for (int i = 0; i < 60*60*12/slice; i++){
+  for (int i = 0; i < 60 * 60 * 12 / slice; i++) {
    clock.tick();
-   if (Math.abs(clock.getAngleBetween() - 180) < (slice/20)){
-    if(alreadyPrinted){System.out.println ("Current time: " + clock + " Current angle: " + clock.getAngleBetween());}
+   if (Math.abs(clock.getAngleBetween() - 180) < (slice / 20)) {
+    if (alreadyPrinted) {
+     System.out.println("Current time: " + clock + " Current angle: " + clock.getAngleBetween());
+    }
     alreadyPrinted = false;
-  // System.out.println ("Minute angle: " + clock.getMinuteAngle());
-      }else{
-       alreadyPrinted = true;
-      } 
+    // System.out.println ("Minute angle: " + clock.getMinuteAngle());
+   } else {
+    alreadyPrinted = true;
+   }
   }
  }
 }
