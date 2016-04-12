4db0f0d4f235af9d63a986c31dcb1d57eae6247c
ddelga13 <ddelga13@lion.lmu.edu>
2016-04-12 16:52:22 -0700

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,18 @@
+public class Circle {
+
+  public Circle (Point center, Double radius){
+    this.center = center;
+    this.radius = radius;
+   
+   }
+
+  public double getCenter(){
+    return this.center;
+  }
+
+  public double getRadius(){
+    return this.radius;
+  }
+
+
+}
