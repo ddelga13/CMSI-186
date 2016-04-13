2af9572355ae22a24cb9786f45daa5a633857c4d
ddelga13 <ddelga13@lion.lmu.edu>
2016-04-12 17:39:03 -0700

    Create RandomizedAreaEstimator.java

---
+++ RandomizedAreaEstimator.java
@@ -0,0 +1,29 @@
+public class RandonizedAreaEstimator {
+  public shape [] ();
+
+
+//shape [] = {triangle, circle circle}
+//Rectangle.getBigBox (smallest x, smallest y, biggest x, biggest y) (use forloop to manually find these)
+
+//generate a million points in big getBigBox
+//intersecting
+//check if the point is in the shape, for each shape in the array
+//then, 
+//output area of just the intersection
+//output area of all the non intersection (union)
+//output area of everything
+//output area of just circle
+//output area of just triangle
+
+//intersection if (if point is in ALL shapes) {
+//then store in intersecting double
+//}
+
+//non-intersecting if (in point is in ONE shape)
+//then store in non-intersecting double
+
+//union if (point is in ANY shape)
+//then store in union double
+
+//shape if (point is in shape)
+//store in individual shape instance (class)
